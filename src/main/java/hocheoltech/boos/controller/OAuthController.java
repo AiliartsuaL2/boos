29717f51b85a52ce8a39922e58bfee1a5761953b
ailiartsua <ailiartsual2@gmail.com>
2023-04-29 16:21:36 +0900

    Oauth 인증 진행중

---
+++ src/main/java/hocheoltech/boos/controller/OAuthController.java
@@ -0,0 +1,10 @@
+package hocheoltech.boos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class OAuthController {
+}
--- src/main/java/hocheoltech/boos/controller/TestController.java
+++ src/main/java/hocheoltech/boos/controller/TestController.java
@@ -1,13 +1,24 @@
 package hocheoltech.boos.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class TestController {
     @PostMapping("/test")
     public String test(){
-
         return "<h1>test 통과</h1>";
+    }
+    @GetMapping("/member/login")
+    public String loginPage(){
+        return "/member/login.html";
+    }
+    @GetMapping("/oauth/kakao/callback")
+    @ResponseBody
+    public String kakaoTest(String code){
+        return "인증완료 코드값 : "+code;
     }
 }
