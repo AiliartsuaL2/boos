0cb2a733dc2ff93bc840626d871c92459f8f9247
ailiartsua <ailiartsual2@gmail.com>
2023-05-07 22:22:54 +0900

    clientId , ClientSecret 정보 숨김(properties.yml 파일로 설정)

---
+++ src/main/java/hocheoltech/boos/oauth/info/OAuthClientInfo.java
@@ -0,0 +1,40 @@
+package hocheoltech.boos.oauth.info;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+// java bean 등록하며 해당 bean 사용시(OAuthInfo enum 에서) 아래의 setter 메서드와 @Value를 이용하여 properties의 값으로 상수 설정
+@Component
+public class OAuthClientInfo {
+    public static String kakaoClientId ;
+    public static String googleClientId ;
+    public static String githubClientId ;
+    public static String kakaoClientSecret ;
+    public static String googleClientSecret ;
+    public static String githubClientSecret ;
+
+    @Value("${oauth.kakao.client-id}")
+    public void setKakaoClientId(String kakaoClientId){
+        this.kakaoClientId = kakaoClientId;
+    }
+    @Value("${oauth.google.client-id}")
+    public void setGoogleClientId(String googleClientId){
+        this.googleClientId = googleClientId;
+    }
+    @Value("${oauth.github.client-id}")
+    public void setGithubClientId(String githubClientId){
+        this.githubClientId = githubClientId;
+    }
+    @Value("${oauth.kakao.client-secret}")
+    public void setKakaoClientSecret(String kakaoClientSecret){
+        this.kakaoClientSecret = kakaoClientSecret;
+    }
+    @Value("${oauth.google.client-secret}")
+    public void setGoogleClientSecret(String googleClientSecret){
+        this.googleClientSecret = googleClientSecret;
+    }
+    @Value("${oauth.github.client-secret}")
+    public void setGithubClientSecret(String githubClientSecret){
+        this.githubClientSecret = githubClientSecret;
+    }
+}
--- src/main/java/hocheoltech/boos/oauth/info/OAuthInfo.java
+++ src/main/java/hocheoltech/boos/oauth/info/OAuthInfo.java
@@ -8,41 +8,33 @@ import lombok.Getter;
 public enum OAuthInfo {
     KAKAO_INFO(
             "authorization_code",
-            "373ab47c76e37b4418bb292d708ce2c0",
+            OAuthClientInfo.kakaoClientId,
+            OAuthClientInfo.kakaoClientSecret,
             "http://localhost:8080/oauth/kakao/login",
-            "3boIieKSCSaBpPfVWI2aNv4Y5yAlIFHS",
             "https://kauth.kakao.com/oauth/token",
             "https://kapi.kakao.com/v2/user/me"
     ),
     GOOGLE_INFO(
             "authorization_code",
-            "900804701090-sk6rt9ah5cp1tmg6ppudj48ki2hs29co.apps.googleusercontent.com",
+            OAuthClientInfo.googleClientId,
+            OAuthClientInfo.googleClientSecret,
             "http://localhost:8080/oauth/google/login",
-            "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",
             "https://oauth2.googleapis.com/token",
             "https://www.googleapis.com/oauth2/v2/userinfo"
     ),
     GITHUB_INFO(
-            null,
-            "Iv1.d98b981d52444256",
+            "authorization_code",
+            OAuthClientInfo.githubClientId,
+            OAuthClientInfo.githubClientSecret,
             "http://localhost:8080/oauth/github/login",
-            "db299ed5f0df814dfaa953d7adc9acbd3ad3f1b1",
             "https://github.com/login/oauth/access_token",
             "https://api.github.com/user"
     );
 
     private final String grantType ;
     private final String clientId ;
-    private final String redirectUri ;
     private final String clientSecret ;
+    private final String redirectUri ;
     private final String tokenUrl ;
     private final String infoUrl ;
-
-    //    @ConfigurationProperties(prefix = "kakao")
-//    public void setKakaoInfo(String clientId, String clientSecret, String redirectUri) {
-//        this.clientId = clientId;
-//        this.clientSecret = clientSecret;
-//        this.redirectUri = redirectUri;
-//    }
-
 }
